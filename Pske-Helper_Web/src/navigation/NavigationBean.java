3bbc51b81a73a2fd5a808aa43ce54267ecb4cc07
Νίκος Γρέτος <ngretos@gmail.com>
2017-10-13 17:03:46 +0300

    Navigation

--- Pske-Helper_Web/src/navigation/NavigationBean.java
+++ Pske-Helper_Web/src/navigation/NavigationBean.java
@@ -12,6 +12,17 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean
 public class NavigationBean {
 	
+	public static final String MAIN_PAGE = "MainLayout.xhtml";
+	
+	public static final String HOME_PAGE = "Home.xhtml";
+
+	private String page;
+
+	public String getMainPage()
+	{
+		return MAIN_PAGE;
+	}
+
 	public String getPageURL(String menuId) {
 		
 		switch (menuId) {
@@ -20,12 +31,39 @@ public class NavigationBean {
 		case "Users":
 			return "Users.xhtml";
 		case "CheckPoints":
-			return "CheckPoints.xhtml";
+			return "Check Points/CheckPoints.xhtml";
 
 		default:
-			return "MainPage.xhtml";
+			return MAIN_PAGE;
 		}
 
+	}
+	
+	public void doNav(String menuId) {
+		
+		switch (menuId) {
+		case "CallPhases":
+			this.page = "CallPhases.xhtml";
+			break;
+		case "Users":
+			this.page = "Users.xhtml";
+			break;
+		case "CheckPoints":
+			this.page = "Check Points/CheckPoints.xhtml";
+			break;
+
+		default:
+			this.page = HOME_PAGE;
+		}
+
+	}
+
+	public String getPage() {
+		return page;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
 	}
 	
 
